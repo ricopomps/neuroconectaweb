be03bfe2648f31286b705b9b7288f8aac472d200
Luiz Pompílio <luiz.pompilio412@gmail.com>
2026-03-30 12:22:19 -0300

    fixing AI audit report

--- src/components/audit-report/audit-log-grid.tsx
+++ src/components/audit-report/audit-log-grid.tsx
@@ -10,113 +10,146 @@ import {
 } from "@/components/ui/select";
 import {
   AiAuditReportFiltersDto,
-  AiProviders,
+  AiFeature,
   AuditLog,
 } from "@/lib/validation/audit-report";
 import * as auditReportApi from "@/network/api/audit-report";
 import { Label } from "@radix-ui/react-label";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { DatePicker } from "../date-picker";
 import { Input } from "../ui/input";
 import { AuditLogList } from "./audit-log-list";
+import { Spinner } from "@/components/ui/spinner";
+import { toast } from "sonner";
 
 interface AuditLogGridProps {
   readonly institutionId: string;
 }
 
 export function AuditLogGrid({ institutionId }: AuditLogGridProps) {
-  const initialValues = {
-    aiProvider: AiProviders.OPEN_AI,
-    model: "4.0",
-    dateFrom: undefined,
-    dateUntil: undefined,
-    userName: "",
-    feature: undefined,
-  };
-
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { isSubmitting },
-  } = useForm<AiAuditReportFiltersDto>({});
+  const { register, control, handleSubmit } = useForm<AiAuditReportFiltersDto>(
+    {},
+  );
 
   const [tableData, setTableData] = useState<AuditLog[]>([]);
+  const [totalTokenInputQty, setTotalTokenInputQty] = useState<number>(0);
+  const [totalTokenOutputQty, setTotalTokenOutputQty] = useState<number>(0);
+
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
   const getReportData = (params: AiAuditReportFiltersDto) => {
     const size = 10;
 
-    auditReportApi.getAuditReportData(params, 1, size).then((response) => {
-      setTableData(response.data);
-    });
+    console.log(params);
+
+    if (
+      params?.dateUntil &&
+      params?.dateFrom &&
+      params.dateUntil < params.dateFrom
+    ) {
+      toast.warning("A data final não deve ser maior do que a data inicial");
+      return;
+    }
+
+    setIsSubmitting(true);
+    auditReportApi
+      .getAuditReportData(params, 1, size)
+      .then((response) => {
+        setTableData(response.data);
+        setTotalTokenInputQty(response.totalTokenInputQty);
+        setTotalTokenOutputQty(response.totalTokenOutputQty);
+      })
+      .finally(() => setIsSubmitting(false));
   };
 
   return (
-    <div className="bg-muted flex min-h-screen items-center justify-center px-4 py-12 sm:px-6 lg:px-8 w-full">
-      <form onSubmit={handleSubmit(getReportData)}>
-        <div className="bg-muted row min-h-screen items-center justify-center px-4 py-12 sm:px-6 lg:px-8 w-full">
-          <div className="grid gap-3">
-            <Label htmlFor="name-1">Usuário</Label>
-            <Input id="name-1" {...register("userName")} />
-          </div>
-          <div className="grid gap-3">
-            <Label htmlFor="model-1">Modelo</Label>
-            <Input id="model-1" {...register("model")} />
-          </div>
-
-          <div className="grid gap-3">
-            <Label htmlFor="ai-provider-1">Provedor da IA</Label>
-            <Select {...register("aiProvider")}>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Theme" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectItem value="Google">Google</SelectItem>
-                  <SelectItem value="OpenAi">OpenAi</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="grid gap-3">
-            <Label htmlFor="feature-1">Funcionalidade</Label>
-            <Select {...register("feature")}>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Theme" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectItem value="Google">Gerar PAEE</SelectItem>
-                  <SelectItem value="OpenAi">Resumir Documentos</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="grid gap-3 mb-4">
-            <DatePicker<AiAuditReportFiltersDto>
-              name="dateFrom"
-              control={control}
-              label="Data Início"
-              maxDate={new Date()}
-              showMonthYearDropdown
-            />
-          </div>
-          <div className="grid gap-3 mb-4">
-            <DatePicker<AiAuditReportFiltersDto>
-              name="dateUntil"
-              control={control}
-              label="Data Fim"
-              maxDate={new Date()}
-              showMonthYearDropdown
-            />
-          </div>
-          <div className="text-right mb-4">
-            <Button type="submit">Buscar</Button>
-          </div>
-          <AuditLogList tableData={tableData} />
+    <div className="bg-muted sm:px-6 lg:px-8 w-full">
+      <div className="flex items-center justify-between px-4 py-12 sm:px-6 lg:px-8 w-full">
+        <div className="grid gap-3">
+          <Label htmlFor="name-1">Usuário</Label>
+          <Input id="name-1" className="w-64" {...register("userName")} />
         </div>
-      </form>
+        <div className="grid gap-3">
+          <Label htmlFor="model-1">Modelo</Label>
+          <Input id="model-1" className="w-64" {...register("model")} />
+        </div>
+
+        <div className="grid gap-3">
+          <Label htmlFor="ai-provider-1">Provedor da IA</Label>
+          <Controller
+            control={control}
+            name="aiProvider"
+            render={({ field }) => (
+              <Select onValueChange={field.onChange} value={field.value}>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Selecione" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="Google">Google</SelectItem>
+                    <SelectItem value="OpenAi">OpenAi</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+          />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="feature-1">Funcionalidade</Label>
+          <Controller
+            control={control}
+            name="feature"
+            render={({ field }) => (
+              <Select onValueChange={field.onChange} value={field.value}>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Selecione" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value={AiFeature.GENERATE_PAEE}>
+                      Gerar PAEE
+                    </SelectItem>
+                    <SelectItem value={AiFeature.SUMMARIZE_TEXT}>
+                      Resumir Documentos
+                    </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+          />
+        </div>
+        <div className="grid gap-3">
+          <DatePicker<AiAuditReportFiltersDto>
+            name="dateFrom"
+            control={control}
+            label="Data Início"
+            maxDate={new Date()}
+            showMonthYearDropdown
+            className="w-64"
+          />
+        </div>
+        <div className="grid gap-3">
+          <DatePicker<AiAuditReportFiltersDto>
+            name="dateUntil"
+            control={control}
+            label="Data Fim"
+            maxDate={new Date()}
+            showMonthYearDropdown
+            className="w-64"
+          />
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <b>
+          Total tokens de entrada: {totalTokenInputQty}, Total tokens de saída:{" "}
+          {totalTokenOutputQty}
+        </b>
+        <Button onClick={handleSubmit(getReportData)} disabled={isSubmitting}>
+          {isSubmitting ? "Buscando..." : "Buscar"}
+        </Button>
+      </div>
+      {isSubmitting ? <Spinner /> : <AuditLogList tableData={tableData} />}
     </div>
   );
 }
